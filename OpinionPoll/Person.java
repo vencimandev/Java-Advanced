79d87b39d5702031355d3c35a972167a960eb9b8
Ventsislav Mandev <115660651+vencimandev@users.noreply.github.com>
2024-02-14 18:45:03 +0200

    Add files via upload

---
+++ OpinionPoll/Person.java
@@ -0,0 +1,25 @@
+package OpinionPoll;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + age;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
